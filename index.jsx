e9cd197c4cb14bdbb3b548c96c9e5f71f0ac2b5b
MIHAL-SHAIKH <156675654+MIHAL-SHAIKH@users.noreply.github.com>
2024-01-21 00:31:37 +0530

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./../src/App.jsx";
+// import App from "./src/App.jsx";
+import App from "./App.jsx";
 
 document.addEventListener("DOMContentLoaded", () => {
   const sections = document.querySelectorAll("section");
